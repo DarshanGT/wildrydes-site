c714650a0bd9ab564ca39afe1ab3f9eef68376e2
Darshan G Tevarimatha <145744969+DarshanGT@users.noreply.github.com>
2025-01-25 11:51:02 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-1_IVpSbc0KF', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '1t9ve0enibler0ji1i27tjrub9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '7sourk3scv8rmfgc8d0je5l6e', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
